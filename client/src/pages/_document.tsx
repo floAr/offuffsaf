9970c0bc42bb8962f6eca395d93f670e6ce164c9
Coding Velociraptor <coding-velociraptor@protonmail.com>
2024-05-26 10:06:08 +0200

    client: page styling

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -3,6 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+      <link href="https://fonts.googleapis.com/css2?family=Poetsen+One&display=swap" rel="stylesheet" />
       <Head />
       <body>
         <Main />
